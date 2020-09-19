1c3d49ebada3b5d53d97f36bacc4113ef55745ba
adishourya <shourya.aditya10@gmail.com>
2020-09-19 18:32:19 +0530

    One Font to Rule em All & Big mpd

---
+++ awesome/widget/mpd/content/song-info.lua
@@ -0,0 +1,75 @@
+local wibox = require('wibox')
+local dpi = require('beautiful').xresources.apply_dpi
+
+local song_info = {}
+
+song_info.music_title = wibox.widget {
+    layout = wibox.layout.fixed.horizontal,
+    expand = 'none',
+	{
+		{
+			id = 'title',
+			text = 'title',
+			font = 'SF Mono Bold 10',
+			align  = 'center',
+			valign = 'center',
+            left=60,
+            right=40,
+			ellipsize = 'end',
+			widget = wibox.widget.textbox
+		},
+		id = 'scroll_container',
+        max_size = 400,
+        speed = 75,
+        expand = true,
+        direction = 'h',
+        left=60,
+        step_function = wibox.container.scroll.step_functions.waiting_nonlinear_back_and_forth,
+        fps = 60,
+        layout = wibox.container.scroll.horizontal
+	}
+}
+
+song_info.music_artist = wibox.widget {
+    layout = wibox.layout.fixed.horizontal,
+    expand = 'none',
+	{
+		{
+			id = 'artist',
+			text = 'artist',
+			font = 'SF Mono Italic 8',
+            left=dpi(24),
+            right=dpi(24),
+			align  = 'center',
+			valign = 'center',
+			widget = wibox.widget.textbox
+		},
+		id = 'scroll_container',
+        max_size = 400,
+        speed = 75,
+        left=dpi(24),
+        right=dpi(24),
+        expand = true,
+        direction = 'h',
+        step_function = wibox.container.scroll
+        				.step_functions.waiting_nonlinear_back_and_forth,
+        fps = 60,
+        layout = wibox.container.scroll.horizontal
+	}
+}
+
+song_info.music_info = wibox.widget {
+	layout = wibox.layout.align.vertical,
+	expand = 'none',
+	nil,
+	{
+		layout = wibox.layout.fixed.vertical,
+		song_info.music_title,
+        left=dpi(24),
+        right=dpi(24),
+		song_info.music_artist
+	},
+	nil
+}
+
+return song_info
---
+++ awesome/widget/xdg-folders/init.lua
@@ -0,0 +1,20 @@
+local awful = require('awful')
+local wibox = require('wibox')
+local beautiful = require('beautiful')
+
+local dpi = require('beautiful').xresources.apply_dpi
+
+local separator =  wibox.widget {
+	orientation = 'horizontal',
+	forced_height = dpi(1),
+	span_ratio = 0.55,
+	widget = wibox.widget.separator
+}
+
+return wibox.widget {
+	layout = wibox.layout.align.vertical,
+  	{
+	 separator,
+		layout = wibox.layout.fixed.vertical
+  	},
+}
